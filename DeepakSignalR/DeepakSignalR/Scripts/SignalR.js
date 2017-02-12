1df76c3a76af66603f7d7da5249cf5408db1c72b
Deepak Mallick <deepak003.d@gmail.com>
2017-02-12 13:45:29 +0530

    Just set up... Nothing Logical done yet

---
+++ DeepakSignalR/DeepakSignalR/Scripts/SignalR.js
@@ -0,0 +1,20 @@
+﻿(function () {
+
+    $.connection.hub.start()
+    .done(function () {
+        console.log("IT WORKED!");
+        $.connection.myHub.server.announce("Message From Server");
+    })
+    .fail(function () {
+        alert("It Failed!");
+    });
+
+    $.connection.myHub.client.announce = function (message) {
+        $("#welcome-messages").append(message + "<br />");
+    }
+
+
+
+
+})()
+
---
+++ DeepakSignalR/DeepakSignalR/Startup.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DeepakSignalR.Startup))]
+namespace DeepakSignalR
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            ConfigureAuth(app);
+
+
+        }
+    }
+}
